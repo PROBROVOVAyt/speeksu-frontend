0fe040ae95c88e7bb2315213679ebc2648ea3f4d
PROBROVOVAyt <probrovova1@gmail.com>
2024-12-31 00:26:44 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.example.com", // для HTTP-запросов
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      },
+      "/ws": {
+        target: "wss://api.example.com", // для WebSocket-соединений
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/ws/, "")
+      }
+    }
+  }
 });
